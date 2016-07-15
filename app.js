727303ba0cadb38e4d65237a920664fad41d94aa
Ashish Kumar Patel <ashish.onmobile@gmail.com>
2016-07-15 20:08:55 +0530

    added Search Form and Sidebar Menu

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var browser_1, core_1;
-    var HelloWorld, UserPanel;
+    var HelloWorld, UserPanel, SearchForm, SidebarMenu;
     return {
         setters:[
             function (browser_1_1) {
@@ -33,6 +33,7 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
                 ], HelloWorld);
                 return HelloWorld;
             }());
+            //--- User Panel for side bar ---//
             UserPanel = (function () {
                 function UserPanel() {
                 }
@@ -45,9 +46,38 @@ System.register(["angular2/platform/browser", "angular2/core"], function(exports
                 ], UserPanel);
                 return UserPanel;
             }());
+            //--- Search Form for side bar ---//
+            SearchForm = (function () {
+                function SearchForm() {
+                }
+                SearchForm = __decorate([
+                    core_1.Component({
+                        selector: 'search-form',
+                        template: "\n   <form action=\"#\" method=\"get\" class=\"sidebar-form\">\n        <div class=\"input-group\">\n          <input type=\"text\" name=\"q\" class=\"form-control\" placeholder=\"Search...\">\n              <span class=\"input-group-btn\">\n                <button type=\"submit\" name=\"search\" id=\"search-btn\" class=\"btn btn-flat\"><i class=\"fa fa-search\"></i>\n                </button>\n              </span>\n        </div>\n      </form>\n "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], SearchForm);
+                return SearchForm;
+            }());
+            //--- Search Form for side bar ---//
+            SidebarMenu = (function () {
+                function SidebarMenu() {
+                }
+                SidebarMenu = __decorate([
+                    core_1.Component({
+                        selector: 'sidebar-menu',
+                        template: "\n    <ul class=\"sidebar-menu\">\n        <li class=\"header\">HEADER</li>\n        <!-- Optionally, you can add icons to the links -->\n        <li class=\"active\"><a href=\"#\"><i class=\"fa fa-link\"></i> <span>Link</span></a></li>\n        <li><a href=\"#\"><i class=\"fa fa-link\"></i> <span>Another Link</span></a></li>\n        <li class=\"treeview\">\n          <a href=\"#\"><i class=\"fa fa-link\"></i> <span>Multilevel</span>\n            <span class=\"pull-right-container\">\n              <i class=\"fa fa-angle-left pull-right\"></i>\n            </span>\n          </a>\n          <ul class=\"treeview-menu\">\n            <li><a href=\"#\">Link in level 2</a></li>\n            <li><a href=\"#\">Link in level 2</a></li>\n          </ul>\n        </li>\n      </ul>\n "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], SidebarMenu);
+                return SidebarMenu;
+            }());
+            //need to bootstrapp all component which we want to display
             browser_1.bootstrap(HelloWorld);
+            browser_1.bootstrap(UserPanel);
+            browser_1.bootstrap(SearchForm);
+            browser_1.bootstrap(SidebarMenu);
         }
     }
 });
-// bootstrap(UserPanel); 
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@
 
 
 
-
+//--- User Panel for side bar ---//
  @Component({
  selector: 'user-panel',
  template: `
@@ -35,7 +35,56 @@
  })
  class UserPanel {
  }
- 
 
+
+
+//--- Search Form for side bar ---//
+ @Component({
+ selector: 'search-form',
+ template: `
+   <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+              <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+ `
+ })
+ class SearchForm {
+ }
+
+ //--- Search Form for side bar ---//
+ @Component({
+ selector: 'sidebar-menu',
+ template: `
+    <ul class="sidebar-menu">
+        <li class="header">HEADER</li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Link in level 2</a></li>
+            <li><a href="#">Link in level 2</a></li>
+          </ul>
+        </li>
+      </ul>
+ `
+ })
+ class SidebarMenu {
+ }
+
+ 
+//need to bootstrapp all component which we want to display
  bootstrap(HelloWorld);
-// bootstrap(UserPanel);
+ bootstrap(UserPanel);
+ bootstrap(SearchForm);
+ bootstrap(SidebarMenu);
